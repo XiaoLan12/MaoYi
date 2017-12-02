84d1195f9d6f96785e15a7af662ce79fbc1239ae
WSLittleBrother <15072477250@163.com>
2017-12-02 18:06:12 +0800

    更新

---
+++ app/src/main/java/com/yizhisha/maoyi/ui/login/activity/RegisterActivity.java
@@ -0,0 +1,25 @@
+package com.yizhisha.maoyi.ui.login.activity;
+
+import com.yizhisha.maoyi.R;
+import com.yizhisha.maoyi.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2017/12/2.
+ */
+
+public class RegisterActivity extends BaseActivity{
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_register;
+    }
+
+    @Override
+    protected void initToolBar() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
---
+++ app/src/main/java/com/yizhisha/maoyi/ui/login/fragment/PhoneLoginFragment.java
@@ -0,0 +1,19 @@
+package com.yizhisha.maoyi.ui.login.fragment;
+
+import com.yizhisha.maoyi.base.BaseFragment;
+
+/**
+ * Created by Administrator on 2017/12/2.
+ */
+
+public class PhoneLoginFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
