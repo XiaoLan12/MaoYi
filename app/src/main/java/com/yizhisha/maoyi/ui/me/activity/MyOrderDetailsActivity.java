9fa016f3e555cb46d32aed557e463d1bcb0392a5
蓝小布 <lanxiaobu11@163.com>
2017-12-07 16:37:38 +0800

    更新

--- app/src/main/java/com/yizhisha/maoyi/ui/me/activity/MyOrderDetailsActivity.java
+++ app/src/main/java/com/yizhisha/maoyi/ui/me/activity/MyOrderDetailsActivity.java
@@ -1,5 +1,7 @@
 package com.yizhisha.maoyi.ui.me.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -16,6 +18,8 @@ import com.yizhisha.maoyi.base.BaseToolbar;
 import com.yizhisha.maoyi.bean.json.MyOrderListBean;
 import com.yizhisha.maoyi.bean.json.PayReqBean;
 import com.yizhisha.maoyi.bean.json.WeChatPayStateBean;
+import com.yizhisha.maoyi.common.dialog.DialogInterface;
+import com.yizhisha.maoyi.common.dialog.NormalAlertDialog;
 import com.yizhisha.maoyi.ui.me.contract.OrderDetailsContract;
 import com.yizhisha.maoyi.ui.me.presenter.OrderDetailsPresenter;
 import com.yizhisha.maoyi.utils.DateUtil;
@@ -28,6 +32,7 @@ import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MyOrderDetailsActivity extends BaseActivity<OrderDetailsPresenter> implements OrderDetailsContract.View {
     @Bind(R.id.toolbar)
@@ -143,13 +148,11 @@ public class MyOrderDetailsActivity extends BaseActivity<OrderDetailsPresenter> 
         }else if(payment==5){
             mTvPayWay.setText("微信支付");
         }*/
+        String express[]=new String[]{"顺丰快递","圆通快递","中通快递","申通快递","韵达快递","京东快递","全峰快递","邮政EMS","安能快递",
+                "百世快递","速尔快递","天天快递","邮政包裹"};
         int express_type = orderList.getExpress_id();
-        if (express_type == 1) {
-            mTvTradelPaymentPay.setText("物流发货");
-        } else if (express_type == 2) {
-            mTvTradelPaymentPay.setText("快递发货");
-        } else if (express_type == 3) {
-            mTvTradelPaymentPay.setText(" 朗通快递");
+        if(express_type>0&&express_type<=express.length) {
+            distributionwayOrderdetailTv.setText(express[express_type - 1]);
         }
         mTvTradelTotal.setText(orderList.getTotalprice() + "");
        /* if(payment==3){
@@ -227,7 +230,46 @@ public class MyOrderDetailsActivity extends BaseActivity<OrderDetailsPresenter> 
     public void loadWeChatPayStateFail(String msg) {
 
     }
+    @OnClick({R.id.contact_the_merchant_ll,R.id.refunds_ll,R.id.confirm_goods_ll,R.id.againbuy_ll,R.id.immediate_payment_ll})
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.contact_the_merchant_ll:
+                new NormalAlertDialog.Builder(this)
+                        .setContentText("124235")
+                        .setContentTextSize(18)
+                        .setLeftText("取消")
+                        .setRightText("呼叫")
+                        .setWidth(0.75f)
+                        .setHeight(0.33f)
+                        .setOnclickListener(new DialogInterface.OnLeftAndRightClickListener<NormalAlertDialog>() {
+                            @Override
+                            public void clickLeftButton(NormalAlertDialog dialog, View view) {
+                                dialog.dismiss();
+                            }
+                            @Override
+                            public void clickRightButton(NormalAlertDialog dialog, View view) {
+                                Intent phoneIneten = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:" + "234325"));
+                                startActivity(phoneIneten);
+                                dialog.dismiss();
 
+                            }
+                        }).build().show();
+                break;
+            case R.id.refunds_ll:
+                Bundle bundle = new Bundle();
+                bundle.putString("ORDERNO", orderList.getOrderno());
+                //bundle.putInt("TYPE", orderList.getGoods().get(0).get);
+                startActivity(ApplyRefundActivity.class,bundle);
+                break;
+            case R.id.confirm_goods_ll:
+                break;
+            case R.id.againbuy_ll:
+                break;
+            case R.id.immediate_payment_ll:
+                break;
+        }
+    }
     /**
      * 根据交易状态，切换布局显示
      *
